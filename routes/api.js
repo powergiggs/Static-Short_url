a134f8b0ab9398d4ed71f0d42fa0ca925d26b26b
powergiggs <kevin@powergiggs.com>
2017-01-08 18:20:19 -0500

    api

--- routes/api.js
+++ routes/api.js
@@ -6,19 +6,19 @@ Kevin Smith
 module.exports = function(express){
 const router = express.Router();
 
+  // generate status check
   router.get('/status', function(req, res){
     res.json({ healthy : true });
 
   });
 
-
+    // generate short url
     router.post ("/url", function(req, res){
       var urls = require("../routes/urlshortener.js");
       var longUrl = req.body.url;
       var shortUrl = "";
-      //req.body.shortenedURL = urls.genurl_Short(longUrl);
       shortUrl = urls.genurl_Short(longUrl);
-      res.json({short_url: shortUrl});
+      res.send({"short_url": shortUrl});
 
 
 
